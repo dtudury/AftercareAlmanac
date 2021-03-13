07e392a8b5d4b432b29f0997faeb1aa234f693f9
David Tudury <david.tudury@gmail.com>
2021-03-13 08:31:43 +0000

    entered history and basic date rendering

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,37 @@
-import { watchFunction, render, h } from './horseless.0.5.2.min.esm.js' // '/unpkg/horseless/horseless.js'
+import { watchFunction, render, h, mapEntries } from './horseless.0.5.2.min.esm.js'
 
+import {model} from './model.js'
+
+const historyAsDays = () => {
+  const output = {}
+  for (const yearName in model.history) {
+    const year = model.history[yearName]
+    for (const monthName in year) {
+      const month = year[monthName]
+      for (const dateName in month) {
+        const person = month[dateName]
+        const date = `${monthName.substr(0, 3)} ${dateName} ${yearName}`
+        output[date] = person
+      }
+    }
+  }
+  return output
+}
+
+const dayNames = [null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const classesForPersonDate = (person, date) => [
+  'day',
+  person,
+  dayNames[new Date(date).getDay()],
+  monthNames[new Date(date).getMonth()]
+].join(' ')
 
 render(document.body, h`
-  going forward
+  ${mapEntries(historyAsDays, (person, date) => h`
+    <div class=${() => classesForPersonDate(person, date)}>
+      <span>${date}</span>: 
+      <span>${person}</span>
+    </div>
+  `)}
 `)
---
+++ docs/model.js
@@ -0,0 +1,222 @@
+import { proxy } from './horseless.0.5.2.min.esm.js'
+
+const Izzy = 'Izzy'
+const Ella = 'Ella'
+const Audrey = 'Audrey'
+const Owen = 'Owen'
+const Barrett = 'Barrett'
+const Sofia = 'Sofia'
+const Tyler = 'Tyler'
+
+const Holiday = 'Holiday'
+const TBD = 'TBD'
+
+
+export const model = window.model = proxy({
+  history: {
+    2020: {
+      august: {
+        10: Ella,
+        11: Audrey,
+        12: Ella,
+        13: Ella,
+        14: Izzy,
+
+        17: Ella,
+        18: Ella,
+        19: Ella,
+        20: Ella,
+        21: Barrett,
+
+        24: Owen,
+        25: Owen,
+        26: Owen,
+        27: Owen,
+        28: Owen,
+
+        31: Izzy
+      },
+      september: {
+        1: Barrett,
+        2: Izzy,
+        3: Izzy,
+        4: Sofia,
+
+        7: Holiday,
+        8: Audrey,
+        9: Izzy,
+        10: Izzy,
+        11: Barrett,
+
+        14: Tyler,
+        15: Tyler,
+        16: Tyler,
+        17: Tyler,
+        18: Barrett,
+
+        21: Owen,
+        22: Owen,
+        23: Owen,
+        24: Owen,
+        25: Izzy,
+
+        28: Tyler,
+        29: Tyler,
+        30: Tyler
+      },
+      october: {
+        1: Tyler,
+        2: Barrett,
+
+        5: Sofia,
+        6: Audrey,
+        7: Izzy,
+        8: Izzy,
+        9: Barrett,
+
+        12: Holiday,
+        13: Sofia,
+        14: Sofia,
+        15: Sofia,
+        16: Barrett,
+
+        19: Owen,
+        20: Audrey,
+        21: Owen,
+        22: Owen,
+        23: Owen,
+
+        26: Ella,
+        27: Ella,
+        28: Holiday,
+        29: Ella,
+        30: Barrett
+      },
+      november: {
+        2: Sofia,
+        3: Audrey,
+        4: Sofia,
+        5: Sofia,
+        6: Barrett,
+
+        9: Sofia,
+        10: Sofia,
+        11: Holiday,
+        12: Tyler,
+        13: Barrett,
+
+        16: Owen,
+        17: Audrey,
+        18: Owen,
+        19: Owen,
+        20: Owen,
+
+        23: Holiday,
+        24: Holiday,
+        25: Holiday,
+        26: Holiday,
+        27: Holiday,
+
+        30: Izzy
+      },
+      december: {
+        1: Audrey,
+        2: Izzy,
+        3: Audrey,
+        4: Sofia,
+
+        7: Tyler,
+        8: Audrey,
+        9: Tyler,
+        10: Tyler,
+        11: Barrett,
+
+        14: Barrett,
+        15: Audrey,
+        16: Holiday,
+        17: Holiday,
+        18: Sofia,
+
+        21: Holiday,
+        22: Holiday,
+        23: Holiday,
+        24: Holiday,
+        25: Holiday,
+
+        28: Holiday,
+        29: Holiday,
+        30: Holiday,
+        31: Holiday,
+      }
+    },
+    2021: {
+      january: {
+        1: Holiday,
+
+        4: Sofia,
+        5: Audrey,
+        6: Sofia,
+        7: Sofia,
+        8: Barrett,
+
+        11: Ella,
+        12: Ella,
+        13: Ella,
+        14: Ella,
+        15: Barrett,
+
+        18: Holiday,
+        19: Izzy,
+        20: Tyler,
+        21: Tyler,
+        22: Barrett,
+
+        25: Owen,
+        26: Audrey,
+        27: Tyler,
+        28: Owen,
+        29: Barrett,
+      },
+      february: {
+        1: Sofia,
+        2: Audrey,
+        3: Sofia,
+        4: Sofia,
+        5: Barrett,
+
+        8: Izzy,
+        9: Audrey,
+        10: Sofia,
+        11: Sofia,
+        12: Barrett,
+
+        15: Holiday,
+        16: Audrey,
+        17: Tyler,
+        18: Tyler,
+        19: Barrett,
+
+        22: Owen,
+        23: Audrey,
+        24: Owen,
+        25: Owen,
+        26: Barrett
+      },
+      march: {
+        1: Ella,
+        2: Ella,
+        3: Ella,
+        4: Ella,
+        5: Barrett,
+
+        8: Sofia,
+        9: Izzy,
+        10: Izzy,
+        11: Izzy,
+        12: Barrett,
+
+        15: TBD
+      }
+    }
+  }
+})
